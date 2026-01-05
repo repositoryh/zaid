3a8d4d2d1177f81c1a2cd5479adca83ab14b25b0
repositoryh <kchs3321@gmail.com>
2026-01-05 05:18:59 +0500

    Update index.ts

--- images/index.ts
+++ images/index.ts
@@ -1,3 +1,4 @@
 import paypalLogo from "./paypalLogo.png";
 import emptyCart from "./emptyCart.png";
-export { paypalLogo, emptyCart };
+import banner_1 from "./banner/banner_1.png";
+export { paypalLogo, emptyCart, banner_1 };
